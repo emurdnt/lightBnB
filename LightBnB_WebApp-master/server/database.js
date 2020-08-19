097230c22d66dc15e105473e199128c0a1510ac5
Em Urdaneta <em.urdaneta94@gmail.com>
2020-08-19 16:23:19 +0000

    create getUserWithEmail, getUserWithId, addUser using PSQL and promises

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,6 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const bcrypt = require('bcrypt');
 
 /// Users
 const { Pool } = require('pg');
@@ -18,16 +19,24 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE email = $1;
+  `;
+  const values = [email]
+
+  return pool.query(queryString, values)
+  .then(res => {
+   if(res.rows.length){
+    return res.rows[0]
+   } else {
+     return null;
+   }
+  }).
+  catch(err => console.error('query error', err.stack));
+
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -37,7 +46,23 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE id = $1;
+  `;
+  const values = [id]
+
+  return pool.query(queryString, values)
+  .then(res => {
+   if(res.rows.length){
+    return res.rows[0]
+   } else {
+     return null;
+   }
+  }).
+  catch(err => console.error('query error', err.stack));
+  // return Promise.resolve(users[id]);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -48,10 +73,34 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  
+  console.log(user);
+  // req.session.userId = user.id;
+  // res.send({user: {name: user.name, email: user.email, id: user.id}});
+  // const newUserId = generateUserId(users);
+
+  //     const inputPassword = password;
+  const name = user['name'];
+  const email = user['email'];
+  const hashedPassword = bcrypt.hashSync(user['password'], 10);
+  const queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES($1,$2,$3)
+  RETURNING *;
+  `;
+
+  const values = [name, email, hashedPassword]
+
+  return pool.query(queryString, values)
+  .then(res => {
+   if(res.rows.length){
+     console.log(res.rows);
+    return res.rows[0]
+   } else {
+     return null;
+   }
+  }).
+  catch(err => console.error('query error', err.stack));
 }
 exports.addUser = addUser;
 
